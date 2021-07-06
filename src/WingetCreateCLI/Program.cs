08f7a277bd3d7825e7db754c52b5e13c1c5ff518
Ryan Fu <ryfu@microsoft.com>
2021-07-06 16:14:51 -0700

    revert program.cs

--- src/WingetCreateCLI/Program.cs
+++ src/WingetCreateCLI/Program.cs
@@ -36,11 +36,7 @@ namespace Microsoft.WingetCreateCLI
             string arguments = string.Join(' ', Environment.GetCommandLineArgs());
             Logger.Trace($"Command line args: {arguments}");
 
-            Parser myParser = new Parser(config =>
-                {
-                    config.HelpWriter = null;
-                    config.IgnoreUnknownArguments = false;
-                });
+            Parser myParser = new Parser(config => config.HelpWriter = null);
 
             var types = GetVerbs();
             var parserResult = myParser.ParseArguments(args, types);
