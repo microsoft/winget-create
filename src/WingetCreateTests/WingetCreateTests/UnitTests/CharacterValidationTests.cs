5bfbd3c5081fb5922d67bf26bf0c171e107569ea
Easton Pillay <easton@planeteaston.com>
2022-07-18 08:41:13 -0400

    Added unit test (thanks @ryfu-msft).

--- src/WingetCreateTests/WingetCreateTests/UnitTests/CharacterValidationTests.cs
+++ src/WingetCreateTests/WingetCreateTests/UnitTests/CharacterValidationTests.cs
@@ -3,10 +3,13 @@
 
 namespace Microsoft.WingetCreateUnitTests
 {
+    using System;
     using System.IO;
+    using System.Linq;
     using Microsoft.WingetCreateCore;
     using Microsoft.WingetCreateCore.Models.Singleton;
     using NUnit.Framework;
+    using WinGetUtil.Models.V1;
 
     /// <summary>
     /// Unit tests for verifying unicode text and directionality.
@@ -26,6 +29,8 @@ namespace Microsoft.WingetCreateUnitTests
             "नमस्ते धन्यवाद",
             "도망각하갂",
         };
+        
+        
 
         /// <summary>
         /// Verifies text support for unicode characters.
@@ -48,5 +53,45 @@ namespace Microsoft.WingetCreateUnitTests
                 File.Delete(testManifestFilePath);
             }
         }
+
+        /// <summary>
+        /// Verifies that we aren't adding more newlines than expected.
+        /// </summary>
+        [Test]
+        public void VerfiyNewLineSupport()
+        {
+            string[] stringsWithNewLines =
+            {
+                "This\n has\n some newlines.",
+                "So\r\n does this.",
+                "And this does\x85.",
+                "As does this\x2028.",
+                "Me too!\x2029:)",
+            };
+
+            string[] delimiters = {"\r\n", "\r", "\n", "\x85", "\x2028", "\x2029"};
+            string testManifestFilePath = Path.Combine(Path.GetTempPath(), "TestManifest.yaml");
+
+            foreach (var i in stringsWithNewLines)
+            {
+                SingletonManifest written = new SingletonManifest {Description = i};
+                File.WriteAllText(testManifestFilePath, written.ToYaml());
+                SingletonManifest read = Serialization.DeserializeFromPath<SingletonManifest>(testManifestFilePath);
+
+                // we know when written that \r\n and \x85 characters are replaced with \n.
+                var writtenFixed = string.Join(
+                    '\n',
+                    written.Description.Split(new string[] {"\n", "\r\n", "\x85"}, StringSplitOptions.None));
+                
+                Assert.AreEqual
+                (
+                    writtenFixed,
+                    read.Description,
+                    $"String {read.Description} had the wrong number of newlines :(."
+                );
+                File.Delete(testManifestFilePath);
+            }
+        }
+        
     }
 }
