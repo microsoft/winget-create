b31251d657a32c51a6ed5293bdc61fc615efb19a
Ryan Fu <Fu.Ryan@microsoft.com>
2023-01-12 10:22:20 -0800

    check if string result is null before setting

--- src/WingetCreateCLI/PromptHelper.cs
+++ src/WingetCreateCLI/PromptHelper.cs
@@ -300,7 +300,11 @@ namespace Microsoft.WingetCreateCLI
             if (instanceType == typeof(string))
             {
                 string result = Prompt.Input<string>(message, property.GetValue(model), new[] { FieldValidation.ValidateProperty(model, memberName, instance) });
-                property.SetValue(model, result.Trim());
+
+                if (!string.IsNullOrEmpty(result))
+                {
+                    property.SetValue(model, result.Trim());
+                }
             }
             else if (instanceType == typeof(long))
             {
