ef2c2e7110cd0006d5586655b3bffcf24d295a7c
Muhammad Danish <mdanishkhdev@gmail.com>
2023-08-07 19:35:29 +0500

    Change LF line endings to CRLF in project files (#418)

--- src/WingetCreateTests/WingetCreateTestExeInstaller/Program.cs
+++ src/WingetCreateTests/WingetCreateTestExeInstaller/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-
-namespace WingetCreateTestExeInstaller
+﻿namespace WingetCreateTestExeInstaller
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
