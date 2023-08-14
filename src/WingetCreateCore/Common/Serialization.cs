b2872127673d39d936ebb8f64f9ab4dac32dfd1c
Muhammad Danish <mdanishkhdev@gmail.com>
2023-08-14 20:17:06 +0500

    Update src/WingetCreateCore/Common/Serialization.cs
    Co-authored-by: Kaleb Luedtke <trenlymc@gmail.com>

--- src/WingetCreateCore/Common/Serialization.cs
+++ src/WingetCreateCore/Common/Serialization.cs
@@ -42,7 +42,7 @@ namespace Microsoft.WingetCreateCore
         {
             var serializer = new SerializerBuilder()
                 .WithNamingConvention(PascalCaseNamingConvention.Instance)
-                .WithDefaultScalarStyle(ScalarStyle.SingleQuoted)
+               .WithQuotingNecessaryStrings()
                 .WithTypeConverter(new YamlStringEnumConverter())
                 .WithEmissionPhaseObjectGraphVisitor(args => new YamlSkipPropertyVisitor(args.InnerVisitor))
                 .WithEventEmitter(nextEmitter => new MultilineScalarFlowStyleEmitter(nextEmitter))
